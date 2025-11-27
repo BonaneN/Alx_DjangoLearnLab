d80e0b6ad3f0273ca6bc536ab15ed0841012a83c
BonaneN <rbonane336@gmail.com>
2025-11-27 07:11:07 +0200

    project setup + serializers

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from datetime import datetime
+
+# Author model: Represents an author who can have many books.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Book model: Each Book is linked to one Author (ForeignKey = one-to-many).
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
