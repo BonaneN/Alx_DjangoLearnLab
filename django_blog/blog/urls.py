f52f7946eee054c8b02193c2c442cc217cc83301
BonaneN <rbonane336@gmail.com>
2025-12-06 15:13:34 +0200

    crud operations urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import register, CustomLoginView, CustomLogoutView, profile
 from .views import (
-    PostListView, PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
+    PostListView, PostDetailView,PostCreateView,PostUpdateView,PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView
 )
 
 urlpatterns = [
@@ -13,5 +13,10 @@ urlpatterns = [
     path("post/new/", PostCreateView.as_view(), name="post_create"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
-    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete")
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+
+    path("post/<int:pk>/comment/", CommentCreateView.as_view(), name="add_comment"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment_update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
+
 ]
