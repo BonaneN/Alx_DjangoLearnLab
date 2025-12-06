82c6434dadf3a1303ece79029593ba083566b23b
BonaneN <rbonane336@gmail.com>
2025-12-06 15:48:23 +0200

    taggit fix

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 from django.db import models
@@ -9,6 +10,8 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
