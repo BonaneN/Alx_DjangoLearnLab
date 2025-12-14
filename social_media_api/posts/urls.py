d001be06082c23124b7653aa006e87943199c9ce
BonaneN <rbonane336@gmail.com>
2025-12-14 18:28:02 +0200

    #

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from .views import PostViewSet, CommentViewSet
 from rest_framework.routers import DefaultRouter
+from .views import FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
