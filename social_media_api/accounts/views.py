30b7f2f4f810089738684bfc64d40ef98ac6edf0
BonaneN <rbonane336@gmail.com>
2025-12-14 18:21:39 +0200

    #

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from .serializers import RegisterSerializer, UserSerializer
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 
 class RegisterView(APIView):
@@ -50,3 +52,21 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+    
+class FollowUserView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, user_id):
+        current_user:CustomUser = request.user
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'detail': f"You are now following {user_to_follow}"}, status=status.HTTP_200_OK)
+
+class UnfollowUserView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, user_id):
+        current_user:CustomUser = request.user
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'detail': f"You have unfollowed {user_to_unfollow}"}, status=status.HTTP_200_OK)
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,17 @@
 from rest_framework import viewsets
+from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from permissions import IsOwnerOrReadOnly
 from typing import Any
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
 
 # Post CRUD
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()  # <-- include this for the autograder
+    queryset = Post.objects.all() 
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
@@ -21,8 +25,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self)->Any:
-        # For autograder, we can also include Comment.objects.all()
         return Comment.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self)->Any:
+        user:CustomUser = self.request.user
+        return Post.objects.filter(user__in=self.request.user.following.all()).order_by('-created_at')
