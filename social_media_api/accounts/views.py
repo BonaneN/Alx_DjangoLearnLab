e6ff86e2a567842c3199c760e61e3632cecd91e8
BonaneN <rbonane336@gmail.com>
2025-12-14 17:19:08 +0200

    fix

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-
 from .serializers import RegisterSerializer, UserSerializer
 
 
