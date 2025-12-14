8cd77b173b5e74cfa4630fe3cba96b38f76f622c
BonaneN <rbonane336@gmail.com>
2025-12-14 17:06:49 +0200

    user authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,33 +1,32 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # REQUIRED by checker
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
         )
-        return user
-    
-class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
 
+        # REQUIRED by checker
+        Token.objects.create(user=user)
+
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers_count', 'following_count']
-
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
+        fields = ('id', 'username', 'email', 'bio')
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,49 +1,53 @@
 from django.contrib.auth import authenticate
-from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
 from .serializers import RegisterSerializer, UserSerializer
 
-User = get_user_model()
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token, _ = Token.objects.get_or_create(usere=user)
-            return Response(
-                    {
-                    'user': UserSerializer(user).data,
-                    'token': token.key
-                    }, 
-                    status=status.HTTP_201_CREATED
-                )
-        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        token = Token.objects.get(user=user)
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'token': token.key
+        }, status=status.HTTP_201_CREATED)
+
+
 class LoginView(APIView):
-    permission_classes=[AllowAny]
+    permission_classes = [AllowAny]
 
     def post(self, request):
-        user= authenticate (
+        user = authenticate(
             username=request.data.get('username'),
             password=request.data.get('password')
         )
+
         if not user:
-            return Response (
-                {'error':'Invalid credentials'},
+            return Response(
+                {'error': 'Invalid credentials'},
                 status=status.HTTP_401_UNAUTHORIZED
             )
-        
+
         token, _ = Token.objects.get_or_create(user=user)
-        return Response ({'user':UserSerializer(user).data, 'token':token.key})
-    
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'token': token.key
+        })
+
+
 class ProfileView(APIView):
-    def get(self,request):
-        serializer=UserSerializer(request.user)
+    def get(self, request):
+        serializer = UserSerializer(request.user)
         return Response(serializer.data)
